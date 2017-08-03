6c1a77177ade5ed747c0896d719a34612c0dfefa
yicone <yicone@gmail.com>
2017-08-03 17:34:35 +0800

    改进: js 修改后，不必在浏览器中手动刷新缓存

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,13 @@
-var gulp = require('gulp'),
-    $ = require('gulp-load-plugins')();
-
+var gulp = require("gulp"),
+  $ = require("gulp-load-plugins")();
 
 //watch
-gulp.task('watch', ['default'], function() {
-    gulp.watch('lib/**/*', ['default']);
+gulp.task("watch", ["default"], function() {
+  gulp.watch("lib/**/*", ["default"]);
 });
 
-
 //default
-gulp.task('default', ['styles', 'js', 'html']);
-
+gulp.task("default", ["reveasy"]);
 
 //////////////////
 
@@ -18,64 +15,79 @@ gulp.task('default', ['styles', 'js', 'html']);
 Styles
  */
 const baseConfig = {
-    baseDir: 'static/',
-    maxSize: 14 * 1024,
-    debug: false
+  baseDir: "static/",
+  maxSize: 14 * 1024,
+  debug: false
 };
 
 //merged css
-gulp.task('styles', ['styles_single'], function() {
-    return gulp.src(['lib/styles/main/*.less', 'lib/styles/main/*.css'])
-        .pipe($.sourcemaps.init())
-        .pipe($.concat('main.css'))
-        .pipe($.less())
-        .pipe($.inlineBase64(baseConfig))
-        .pipe($.autoprefixer({
-            browsers: ['> 5%']
-        }))
-        .pipe($.sourcemaps.write('.'))
-        .pipe(gulp.dest('dist/styles'));
+gulp.task("styles", ["styles_single"], function() {
+  return gulp
+    .src(["lib/styles/main/*.less", "lib/styles/main/*.css"])
+    .pipe($.sourcemaps.init())
+    .pipe($.concat("main.css"))
+    .pipe($.less())
+    .pipe($.inlineBase64(baseConfig))
+    .pipe(
+      $.autoprefixer({
+        browsers: ["> 5%"]
+      })
+    )
+    .pipe($.sourcemaps.write("."))
+    .pipe(gulp.dest("dist/styles"));
 });
 
 //single css files
-gulp.task('styles_single', function() {
-    return gulp.src(['lib/styles/*.less', 'lib/styles/*.css'])
-        .pipe($.sourcemaps.init())
-        .pipe($.less())
-        .pipe($.inlineBase64(baseConfig))
-        .pipe($.autoprefixer({
-            browsers: ['> 5%']
-        }))
-        .pipe($.sourcemaps.write('.'))
-        .pipe(gulp.dest('dist/styles'));
+gulp.task("styles_single", function() {
+  return gulp
+    .src(["lib/styles/*.less", "lib/styles/*.css"])
+    .pipe($.sourcemaps.init())
+    .pipe($.less())
+    .pipe($.inlineBase64(baseConfig))
+    .pipe(
+      $.autoprefixer({
+        browsers: ["> 5%"]
+      })
+    )
+    .pipe($.sourcemaps.write("."))
+    .pipe(gulp.dest("dist/styles"));
 });
-
 
 /*
 js
  */
-gulp.task('js', function() {
-    return gulp.src('lib/js/*.js')
-        .pipe($.sourcemaps.init())
-        .pipe($.babel({
-            presets: ['es2015']
-        }))
-        //.pipe($.concat('main.js'))
-        .pipe($.uglify())
-        .pipe($.sourcemaps.write('.'))
-        .pipe(gulp.dest('dist/js'));
+gulp.task("js", function() {
+  return (gulp
+      .src("lib/js/*.js")
+      .pipe($.sourcemaps.init())
+      .pipe(
+        $.babel({
+          presets: ["es2015"]
+        })
+      )
+      //.pipe($.concat('main.js'))
+      .pipe($.uglify())
+      .pipe($.sourcemaps.write("."))
+      .pipe(gulp.dest("dist/js")) );
 });
-
 
 /*
 html
  */
-gulp.task('html', ['styles', 'js'], function() {
-    return gulp.src('lib/index.html')
-        .pipe($.inlineSource({rootpath:'.'}))
-        .pipe($.htmlmin({
-            collapseWhitespace: true,
-            sortAttributes: true
-        }))
-        .pipe(gulp.dest('.'));
+gulp.task("html", ["styles", "js"], function() {
+  return gulp
+    .src("lib/index.html")
+    .pipe($.inlineSource({ rootpath: "." }))
+    .pipe(
+      $.htmlmin({
+        collapseWhitespace: true,
+        sortAttributes: true
+      })
+    )
+    .pipe(gulp.dest("."));
+});
+
+// 用带版本号的资源路径替换不带版本号的资源路径
+gulp.task("reveasy", ["html"], function() {
+  return gulp.src("index.html").pipe($.revEasy({})).pipe(gulp.dest("."));
 });
