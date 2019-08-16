7224c67ade0508f32776758d53661a9f2f06f9a2
Terrance <Tr@Terrances-MacBook-Pro.local>
2019-08-16 18:19:19 +0800

    Add Twitter Upgrade Gulp for npm security tips; Fix gulp-inline-base64 and mime compatibility issues;

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,6 @@
 var gulp = require("gulp"),
   $ = require("gulp-load-plugins")();
 
-//watch
-gulp.task("watch", ["default"], function() {
-  gulp.watch("lib/**/*", ["default"]);
-});
-
-//default
-gulp.task("default", ["reveasy"]);
 
 //////////////////
 
@@ -20,25 +13,8 @@ const baseConfig = {
   debug: false
 };
 
-//merged css
-gulp.task("styles", ["styles_single"], function() {
-  return gulp
-    .src(["lib/styles/main/*.less", "lib/styles/main/*.css"])
-    .pipe($.sourcemaps.init())
-    .pipe($.concat("main.css"))
-    .pipe($.less())
-    .pipe($.inlineBase64(baseConfig))
-    .pipe(
-      $.autoprefixer({
-        browsers: ["> 5%"]
-      })
-    )
-    .pipe($.sourcemaps.write("."))
-    .pipe(gulp.dest("dist/styles"));
-});
-
 //single css files
-gulp.task("styles_single", function() {
+gulp.task("styles_single", function () {
   return gulp
     .src(["lib/styles/*.less", "lib/styles/*.css"])
     .pipe($.sourcemaps.init())
@@ -53,28 +29,44 @@ gulp.task("styles_single", function() {
     .pipe(gulp.dest("dist/styles"));
 });
 
+//merged css
+gulp.task("styles", function () {
+  return gulp
+    .src(["lib/styles/main/*.less", "lib/styles/main/*.css"])
+    .pipe($.sourcemaps.init())
+    .pipe($.concat("main.css"))
+    .pipe($.less())
+    .pipe($.inlineBase64(baseConfig))
+    .pipe(
+      $.autoprefixer({
+        browsers: ["> 5%"]
+      })
+    )
+    .pipe($.sourcemaps.write("."))
+    .pipe(gulp.dest("dist/styles"));
+});
 /*
 js
  */
-gulp.task("js", function() {
+gulp.task("js", function () {
   return (gulp
-      .src("lib/js/*.js")
-      .pipe($.sourcemaps.init())
-      .pipe(
-        $.babel({
-          presets: ["es2015"]
-        })
-      )
-      //.pipe($.concat('main.js'))
-      .pipe($.uglify())
-      .pipe($.sourcemaps.write("."))
-      .pipe(gulp.dest("dist/js")) );
+    .src("lib/js/*.js")
+    .pipe($.sourcemaps.init())
+    .pipe(
+      $.babel({
+        presets: ["es2015"]
+      })
+    )
+    //.pipe($.concat('main.js'))
+    .pipe($.uglify())
+    .pipe($.sourcemaps.write("."))
+    .pipe(gulp.dest("dist/js")));
 });
 
 /*
 html
  */
-gulp.task("html", ["styles", "js"], function() {
+gulp.task("html", gulp.series("styles", "js", function () {
   return gulp
     .src("lib/index.html")
     .pipe($.inlineSource({ rootpath: "." }))
@@ -85,10 +77,10 @@ gulp.task("html", ["styles", "js"], function() {
       })
     )
     .pipe(gulp.dest("."));
-});
+}));
 
 // 用带版本号的资源路径替换不带版本号的资源路径
-gulp.task("reveasy", ["html"], function() {
+gulp.task("reveasy", gulp.series("html", function () {
   return gulp
     .src("index.html")
     .pipe(
@@ -101,4 +93,18 @@ gulp.task("reveasy", ["html"], function() {
       })
     )
     .pipe(gulp.dest("."));
-});
+}));
+
+//default
+gulp.task("default", gulp.series("reveasy", function (done) { done() }));
+
+//watch
+// gulp.task("watch", gulp.series("default", function (done) {
+//   return 
+// }));
+
+function watch () {
+  gulp.watch("lib/**/*", gulp.series("default"));
+}
+
+exports.watch = watch
